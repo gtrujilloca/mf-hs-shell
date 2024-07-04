0e446652dc55cf4b7f24e63c8f801ccf7c8d76b4
carlosg <carlos.garciatru@pragma.com.co>
2024-07-04 18:28:14 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: 'products',
+  //   loadComponent: () => loadRemoteModule('shop', './Products')
+  // },
+  {
+    path: 'products',
+    loadComponent: () => loadRemoteModule({
+      remoteName: 'shop',
+      // remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      exposedModule: './Products'
+    }).then(m => m.ProductsComponent)
+  },
+  // {
+  //   path: 'product/:id',
+  //   loadComponent: () => loadRemoteModule({
+  //     remoteName: 'shop',
+  //     // remoteEntry: 'http://localhost:4201/remoteEntry.js',
+  //     exposedModule: './Product'
+  //   })
+  // },
+  // {
+  //   path: 'checkout',
+  //   loadComponent: () => loadRemoteModule({
+  //     remoteName: 'checkout',
+  //     // remoteEntry: 'http://localhost:4202/remoteEntry.js',
+  //     exposedModule: './Checkout'
+  //   })
+  // }
+];
